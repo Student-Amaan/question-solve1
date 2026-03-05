821f15993174902a601700ed6a5912d7fa36fe23
waxir <danishdarman06@gmail.com>
2026-03-05 22:57:02 +0500

    array sum function even odd number function is commited

--- app.js
+++ app.js
@@ -4,3 +4,29 @@ function reverseString(str) {
 
 
 console.log(reverseString("Aman danish"))
+
+// check even or odd
+
+let num = 20;
+
+if (num % 2 === 0){
+    console.log('the number is even')
+}else{
+    console.log('the number is odd')
+}
+
+let array = [1,5,3,6,7,5,4,7]
+array.push(8)
+
+console.log(array)
+
+function sumArr(arr){
+    let sum = 0
+
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    return sum
+}
+
+console.log(sumArr([1,3,4,6,7,5,8,9]))
